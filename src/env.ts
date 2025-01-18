42d4061eb0c70c6b8c49adec43f05362fe9e9cd9
Vitor Nere <vitor.nere@hotmail.com>
2025-01-17 23:43:25 -0300

    Lint

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,15 @@
-import { z } from 'zod';
+import { z } from 'zod'
 
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
-  NODE_ENV: z.enum(['test', 'development', 'production']).default('development'),
-  DATABASE_URL: z.string().url().startsWith('postgres://').default('postgresql://docker:docker@localhost:5432/upload'),
-});
+  NODE_ENV: z
+    .enum(['test', 'development', 'production'])
+    .default('development'),
+  DATABASE_URL: z
+    .string()
+    .url()
+    .startsWith('postgres://')
+    .default('postgresql://docker:docker@localhost:5432/upload'),
+})
 
-export const env = envSchema.parse(process.env);
+export const env = envSchema.parse(process.env)
