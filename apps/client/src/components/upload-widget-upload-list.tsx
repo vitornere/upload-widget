21d46352ea1746f23ac2144f5a751c5dd92f4702
Vitor Nere <vitor.nere@hotmail.com>
2025-02-11 12:51:27 -0300

    add upload list and items

--- apps/client/src/components/upload-widget-upload-list.tsx
+++ apps/client/src/components/upload-widget-upload-list.tsx
@@ -1,7 +1,17 @@
+import { UploadWidgetUploadItem } from "./upload-widget-upload-item";
+
 export function UploadWidgetUploadList() {
     return (
-        <div className="w-full h-full flex items-center justify-center">
-            <span>Upload List</span>
+        <div className="px-3 flex flex-col gap-3">
+            <span className="text-xs font-medium">
+                Uploaded files{' '}
+                <span className="text-zinc-400">(2)</span>
+            </span>
+
+            <div className="flex flex-col gap-2">
+                <UploadWidgetUploadItem />
+                <UploadWidgetUploadItem />
+            </div>
         </div>
     )
 }
