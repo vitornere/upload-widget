5b1e253344309c1fc9ba56a6051d0cf228b5454b
Vitor Nere <vitor.nere@hotmail.com>
2025-02-14 11:03:24 -0300

    add circular progress

--- apps/client/src/components/upload-widget-upload-list.tsx
+++ apps/client/src/components/upload-widget-upload-list.tsx
@@ -1,6 +1,8 @@
 import { UploadWidgetUploadItem } from "./upload-widget-upload-item";
 
 export function UploadWidgetUploadList() {
+    const isUploadListEmpty = false
+
     return (
         <div className="px-3 flex flex-col gap-3">
             <span className="text-xs font-medium">
@@ -8,10 +10,14 @@ export function UploadWidgetUploadList() {
                 <span className="text-zinc-400">(2)</span>
             </span>
 
-            <div className="flex flex-col gap-2">
-                <UploadWidgetUploadItem />
-                <UploadWidgetUploadItem />
-            </div>
+            {isUploadListEmpty ? (
+                <span className="text-xs text-zinc-400">No uploads added</span>
+            ) : (
+                <div className="flex flex-col gap-2">
+                    <UploadWidgetUploadItem />
+                    <UploadWidgetUploadItem />
+                </div>
+            )}
         </div>
     )
 }
